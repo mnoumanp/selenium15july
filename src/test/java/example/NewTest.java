9664a3dbe2c621106ec77b6e9418c4a820832e68
Ghanekar <kishor.ghanekar@capgemini.com>
2019-07-15 20:46:30 +0530

    first commit

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,43 @@
+package example;
+
+
+import static org.testng.AssertJUnit.assertTrue;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	private WebDriver driver;		
+	@Test				
+	public void testEasy() {	
+		/*driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); */	
+		driver.get("https://www.saucelabs.com");       
+	       WebElement href = driver.findElement(By.xpath("//a[@href='/beta/login']"));
+	       assertTrue((href.isDisplayed()));  
+	       driver.close();
+		
+	}	
+	@BeforeTest
+	public void beforeTest() {	
+	   // driver = new FirefoxDriver(); 
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\kghaneka\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();  
+		//testEasy();
+		/*driver.get("https://www.saucelabs.com");       
+	       WebElement href = driver.findElement(By.xpath("//a[@href='/beta/login']"));
+	       assertTrue((href.isDisplayed()));  
+	       driver.close();*/
+	}		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}	
+}
